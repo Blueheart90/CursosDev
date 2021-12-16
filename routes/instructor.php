220780625e95ca55b6d0867521c9b9d76ebfbd53
Blueheart90 <chuchober@hotmail.com>
2021-12-16 11:02:22 -0500

    Creando rutas para instructores

---
+++ routes/instructor.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Livewire\InstructorCourses;
+use Illuminate\Support\Facades\Route;
+
+// Todos los 'names' de la rutas empiezan con 'intructor.' Ej: 'instructor.courses.index'
+
+Route::redirect('', 'instructor/courses');
+
+Route::get('/courses', InstructorCourses::class )->name('courses.index');
+
