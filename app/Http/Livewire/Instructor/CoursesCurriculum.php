9cafa7ac48af2f5a51cf2da4c157c76c66b7159e
Blueheart90 <chuchober@hotmail.com>
2021-12-22 10:10:05 -0500

    CRUD secciones del curso

--- app/Http/Livewire/Instructor/CoursesCurriculum.php
+++ app/Http/Livewire/Instructor/CoursesCurriculum.php
@@ -3,15 +3,61 @@
 namespace App\Http\Livewire\Instructor;
 
 use App\Models\Course;
+use App\Models\Section;
 use Livewire\Component;
+use Illuminate\Http\Request;
 
 class CoursesCurriculum extends Component
 {
-    public $course;
+    public $course, $section, $newSectionName;
+
+    protected $rules = [
+        'section.name' => 'required'
+    ];
 
     public function mount(Course $course)
     {
        $this->course = $course;
+       $this->section = new Section();
+    }
+
+    public function edit(Section $section)
+    {
+        $this->section = $section;
+    }
+
+    public function update()
+    {
+        $this->validate();
+
+        $this->section->save();
+
+        // Reseteamos la propiedad section para que el input no se muestre
+        $this->section = new Section();
+
+        // Recuperamos el resgistro nuevo de course para que se actualize en la vista
+        $this->course = Course::find($this->course->id);
+    }
+
+    public function store()
+    {
+        $this->validate([
+            'newSectionName' => 'required'
+        ]);
+        
+        Section::create([
+            'name' => $this->newSectionName,
+            'course_id' => $this->course->id
+        ]);
+
+        $this->reset('newSectionName');
+        $this->course = Course::find($this->course->id);
+    }
+
+    public function destroy(Section $section)
+    {
+        $section->delete();
+        $this->course = Course::find($this->course->id);
     }
     
     public function render()
--- resources/views/livewire/instructor/courses-curriculum.blade.php
+++ resources/views/livewire/instructor/courses-curriculum.blade.php
@@ -2,5 +2,60 @@
     <x-slot name="course">
         {{$course->slug}}
     </x-slot>
-    hola
+    <h1 class="text-2xl font-bold uppercase ">Lecciones del curso</h1>
+    <br class="mt-2 mb-6 ">
+
+    @forelse ($course->sections as $item)
+        
+        <article class="mb-6 card">
+            <div class="bg-gray-100 card-body">
+                @if ($section->is($item))
+                    <form wire:submit.prevent='update'>
+                        <input wire:model='section.name' class="w-full rounded-md " placeholder="Ingrese el nombre de la sección" type="text">
+                        @error('section.name')
+                            <span class="text-xs text-red-500 ">{{ $message }}</span>
+                        @enderror
+                    </form>
+                @else
+                    <header class="flex items-center justify-between">
+                        <h1 class="cursor-pointer "><strong>Sección: </strong>{{$item->name}}</h1>
+                        <div>
+                            <i wire:click='edit({{ $item }})' class="text-blue-500 cursor-pointer fas fa-edit"></i>
+                            <i wire:click='destroy({{ $item }})' class="text-red-500 cursor-pointer fas fa-eraser"></i>
+                        </div>
+                    </header>
+                
+                    
+                @endif
+
+            </div>
+        </article>
+    @empty
+       <p> No hay lecciones aun.</p>
+    @endforelse
+    <div x-data="{ open: false }">
+        <a x-show="!open" @click="open = !open" class="flex items-center cursor-pointer ">
+            <i class="mr-2 text-2xl text-green-500 far fa-plus-square"></i>
+            Nueva sección
+        </a>
+        <article class="card" x-show="open" x-cloak>
+            <div class="card-body">
+                <div class="bg-gray-100 card-body">
+                    <h1 class="mb-4 text-xl font-bold">Agregar nueva sección</h1>
+                    <div>
+                        <input wire:model="newSectionName" class="w-full rounded-md" placeholder="Ingrese el nombre de la sección" type="text">
+                        @error('newSectionName')
+                            <span class="text-xs text-red-500 ">{{ $message }}</span>
+                        @enderror
+                    </div>
+                    <div class="flex justify-end mt-2">
+                        <button wire:click='store' class="px-4 py-2 ml-2 text-white bg-blue-500 rounded-md hover:bg-blue-600">Agregar</button>
+                        <button @click="open = false" class="px-4 py-2 ml-2 text-white bg-red-500 rounded-md hover:bg-red-600">Cancelar</button>
+
+                    </div>
+                </div>
+    
+            </div>
+        </article>
+    </div>
 </div>
