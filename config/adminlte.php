bca6859b225ceeccad3a9f58cee81e4a04b0b417
Blueheart90 <chuchober@hotmail.com>
2021-12-31 15:45:40 -0500

    crud levels

--- config/adminlte.php
+++ config/adminlte.php
@@ -273,6 +273,11 @@ return [
             'icon' => 'fas fa-fw fa-cogs',
         ],
         [
+            'text' => 'Niveles',
+            'route'  => 'admin.levels.index',
+            'icon' => 'fas fa-fw fa-cogs',
+        ],
+        [
             'text' => 'pending_approval',
             'route'  => 'admin.courses.index',
             'icon' => 'fas fa-fw fa-user',
--- routes/admin.php
+++ routes/admin.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\CourseController;
 use App\Http\Controllers\Admin\HomeController;
+use App\Http\Controllers\Admin\LevelController;
 use App\Http\Controllers\Admin\RoleController;
 use App\Http\Controllers\Admin\UserController;
 use Illuminate\Support\Facades\Route;
@@ -12,7 +13,9 @@ Route::get('/',[HomeController::class, 'index'])->middleware('can:Ver dashboard'
 
 Route::resource('roles', RoleController::class)->names('roles');
 Route::resource('users', UserController::class)->only(['index', 'edit', 'update'])->names('users');
+
 Route::resource('categories', CategoryController::class)->names('categories');
+Route::resource('levels', LevelController::class)->names('levels');
 
 Route::get('courses', [CourseController::class, 'index'])->name('courses.index');
 Route::get('courses/{course}', [CourseController::class, 'show'])->name('courses.show');
